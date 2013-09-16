9d23114fe250ab6e166d8e396d0a60b9d53bc73f
mkryger <joyfulcynic1@gmail.com>
2013-09-16 00:39:52 -0700

    Beginnings

---
+++ FemtoControl/FemtoControl/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace FemtoControl
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Run_Scripts = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Run_Scripts
+            // 
+            this.Run_Scripts.Location = new System.Drawing.Point(12, 12);
+            this.Run_Scripts.Name = "Run_Scripts";
+            this.Run_Scripts.Size = new System.Drawing.Size(75, 23);
+            this.Run_Scripts.TabIndex = 0;
+            this.Run_Scripts.Text = "Run_Scripts";
+            this.Run_Scripts.UseVisualStyleBackColor = true;
+            this.Run_Scripts.Click += new System.EventHandler(this.Run_Scripts_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(608, 14);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(415, 457);
+            this.textBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1035, 483);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Run_Scripts);
+            this.Name = "Form1";
+            this.Text = "FemtoControl";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Run_Scripts;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ FemtoControl/FemtoControl/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Threading;
+
+namespace FemtoControl
+{
+    public partial class Form1 : Form
+    {
+        
+
+
+        public Form1()
+        {
+            InitializeComponent();              
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void Run_Scripts_Click(object sender, EventArgs e)
+        {
+            // This is where we'll keep the test scripting.  
+            this.textBox1.AppendText("Please Wait while Equipment is Detected...\n");
+
+            // Create length '2' array where we can store references to the Serial Port Objects
+            SerialPort XY_CONTROLLER = new SerialPort(); SerialPort AOI_CONTROLLER = new SerialPort(); SerialPort TOOL_PORT = new SerialPort();
+
+              
+            // Output to textbox1 as a pseudo command prompt so we can keep track of what's happening... keep trying to get attached console thing to work...
+            this.textBox1.AppendText("Available COM Ports:\n");
+            foreach (string s in SerialPort.GetPortNames())
+            {
+                this.textBox1.AppendText("COM port:  " + s + "\n");
+            }
+
+
+
+            
+
+            
+
+        }
+
+    }
+}
